b0fc30c0a0d2a4535eeb1444e41e3733a6e09e75
Marc Newport <marcnewport@gmail.com>
2023-10-18 22:07:27 +0200

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -16,5 +16,5 @@ export function Layout({ header, children }) {
   )
 }
 export function Footer() {
-  return <p>©Copyright 2050 by nobody. All rights reversed.</p>
+  return <p className="p-4">©Copyright 2050 by nobody. All rights reversed.</p>
 }
