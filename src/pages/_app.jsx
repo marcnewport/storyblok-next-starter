f120d38c803f8331e619b7c8feb11f67d387f3db
Marc Newport <marcnewport@gmail.com>
2023-10-19 18:28:28 +0200

    add editable sitename and logo to header

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,20 +4,20 @@ import { Layout } from '@/components/layout'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  const { story, ...props } = pageProps
+  const { story, header } = pageProps
+
   // TODO : meta tags
+  const siteName = header.content.site_name
+  const pageName = story.content.title ?? story.name
+  const title = [pageName, siteName].filter((it) => !!it).join(' | ')
 
   return (
     <StoryblokProvider>
       <Head>
-        <title>
-          {story.content.title ?? story.name ?? 'Storyblok Page'}
-          {' | '}
-          {'Site Name'}
-        </title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout {...props}>
+      <Layout header={header}>
         <Component story={story} />
       </Layout>
     </StoryblokProvider>
